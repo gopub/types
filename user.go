f6d99fa6ef850fb0a35749c8fdec1fe9c98ddd89
JustinTan <mrnatande@gmail.com>
2020-03-21 19:16:15 +0800

    SQL support

--- user.go
+++ user.go
@@ -1,7 +1,10 @@
 package types
 
 import (
+	"database/sql"
+	"database/sql/driver"
 	"fmt"
+	"strings"
 )
 
 // FullName defines user's full name
@@ -11,8 +14,49 @@ type FullName struct {
 	LastName   string `json:"last_name"`
 }
 
+var _ driver.Valuer = (*FullName)(nil)
+var _ sql.Scanner = (*FullName)(nil)
+
 func (n *FullName) String() string {
 	return fmt.Sprintf("%s %s %s", n.FirstName, n.MiddleName, n.LastName)
+}
+
+// Scan implements sql.Scanner
+func (n *FullName) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+
+	s, ok := src.(string)
+	if !ok {
+		var b []byte
+		b, ok = src.([]byte)
+		if ok {
+			s = string(b)
+		}
+	}
+
+	if !ok || len(s) < 4 {
+		return fmt.Errorf("failed to parse %v into gox.PhoneNumber", src)
+	}
+
+	s = s[1 : len(s)-1]
+	segments := strings.Split(s, ",")
+	if len(segments) != 3 {
+		return fmt.Errorf("failed to parse %v into gox.PhoneNumber", src)
+	}
+
+	n.FirstName, n.MiddleName, n.LastName = segments[0], segments[1], segments[2]
+	return nil
+}
+
+// Value implements driver.Valuer
+func (n *FullName) Value() (driver.Value, error) {
+	if n == nil {
+		return nil, nil
+	}
+	s := fmt.Sprintf("(%s,%s,%s)", n.FirstName, n.MiddleName, n.LastName)
+	return s, nil
 }
 
 type Gender int
