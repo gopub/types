2d81ddc97aafc96d670773df045e4366df250816
JustinTan <mrnatande@gmail.com>
2020-03-31 20:46:59 +0800

    Add NewCounter

--- counter.go
+++ counter.go
@@ -6,6 +6,10 @@ type Counter struct {
 	count int64
 }
 
+func NewCounter(start int64) *Counter {
+	return &Counter{count: start}
+}
+
 func (c *Counter) Next() int64 {
 	atomic.AddInt64(&c.count, 1)
 	return c.count
